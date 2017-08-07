a1dde91b619549f7de50bf528361c62ad1283a8c
Supriya Sanjay <supriya.sanjay97@gmail.com>
2017-08-07 15:06:57 -0700

    print all finished

--- ROI_calc.js
+++ ROI_calc.js
@@ -3,17 +3,23 @@ function Calculate() {
 				var labeled_input = new Array();
 				labeled_input = set_data(); // Set data
 				parse_input(labeled_input); // Parse types of venues
-				display();
+				display_all();
 
-				function display() {
-					var responses = document.getElementById("responses");
-					if (responses.style.display == 'none') {
-						responses.style.display = 'block';
+				function display_all() {
+					var element_ID = ["Total-ROI","Ticket-Sales","Online-Sales","Email-marketing","automation","donations","Social-Media"];
+					for (var i = 0; i < 7; i++) {
+
+						var final_ROI = document.getElementById(element_ID[i]);
+						if (final_ROI.style.display == 'none') {
+							final_ROI.style.display = 'block';
+						}
+						else {
+							final_ROI.style.display = 'none';
+						}	
 					}
-					else {
-						responses.style.display = 'none';
-					}
+					
 				}
+
 				// Get data inputted and put in a labeled input array.
 				function set_data() {
 
@@ -35,26 +41,26 @@ function Calculate() {
 
 						labeled_input["Type"] = 2;
 						console.log("Found live music venue");
-						//call live music
+						Live_Music(labeled_input);
 
 					}
 					else if (labeled_input["Type"] == "Performing Arts") {
 
 						labeled_input["Type"] = 3;
 						console.log("Found PA");
-						//call performing arts 
+						Performing_Arts(labeled_input);
 					}
 					else if (labeled_input["Type"] == "Festival") {
 
 						labeled_input["Type"] = 4;
 						console.log("Found festival");
-						//festival 
+						Festival(labeled_input);
 					}
 					else if (labeled_input["Type"] == "University") {
 
 						labeled_input["Type"] = 5;
 						console.log("Found university");
-						// university 
+						University(labeled_input);
 					}
 					else {
 
@@ -145,6 +151,7 @@ function Calculate() {
 
 					ROI_breakdown = new Array();
 					var Total_ROI = 0;
+
 					ROI_breakdown["Ticket_Sales"] = ticket_sales(labeled_input["orig_ROI"]);
 					document.getElementById("Ticket_Sales").textContent = ROI_breakdown["Ticket_Sales"];
 					Total_ROI += ROI_breakdown["Ticket_Sales"];
@@ -173,5 +180,115 @@ function Calculate() {
 					ROI_breakdown["Total"] = Total_ROI;
 					document.getElementById("Total_ROI").textContent = Total_ROI;
 				}
+
+				function Performing_Arts(labeled_input) {
+
+					ROI_breakdown = new Array();
+					var Total_ROI = 0;
+
+					ROI_breakdown["Ticket_Sales"] = ticket_sales(labeled_input["orig_ROI"]);
+					document.getElementById("Ticket_Sales").textContent = ROI_breakdown["Ticket_Sales"];
+					Total_ROI += ROI_breakdown["Ticket_Sales"];
+
+					ROI_breakdown["Automation"] = automation(labeled_input["Employee_wage"], 10);
+					document.getElementById("Automation").textContent = ROI_breakdown["Automation"];
+					Total_ROI += ROI_breakdown["Automation"];
+
+
+					ROI_breakdown["Social_Media"] = social_media(labeled_input["TPY"], labeled_input["Type"]);
+					document.getElementById("Social_Media").textContent = ROI_breakdown["Social_Media"];
+					Total_ROI += ROI_breakdown["Social_Media"];
+
+					ROI_breakdown["Online_Sales"] = online_sales(labeled_input["TPY"], labeled_input["TPrice"], .80);
+					document.getElementById("Online_Sales").textContent = ROI_breakdown["Online_Sales"];
+					Total_ROI += ROI_breakdown["Online_Sales"];
+
+					ROI_breakdown["Email_marketing"] = marketing(labeled_input["Email_marketing"], labeled_input["TPY"]);
+					document.getElementById("Email_marketing").textContent = ROI_breakdown["Email_marketing"];
+					Total_ROI += ROI_breakdown["Email_marketing"];
+
+					ROI_breakdown["Donations"] = donations(labeled_input["TPY"]);
+					document.getElementById("Donations").textContent = ROI_breakdown["Donations"];
+					Total_ROI += ROI_breakdown["Donations"];
+
+					ROI_breakdown["Total"] = Total_ROI;
+					document.getElementById("Total_ROI").textContent = Total_ROI;
+				}
+
+				function University(labeled_input) {
+
+					ROI_breakdown = new Array();
+					var Total_ROI = 0;
+
+					ROI_breakdown["Ticket_Sales"] = ticket_sales(labeled_input["orig_ROI"]);
+					document.getElementById("Ticket_Sales").textContent = ROI_breakdown["Ticket_Sales"];
+					Total_ROI += ROI_breakdown["Ticket_Sales"];
+
+					ROI_breakdown["Automation"] = automation(labeled_input["Employee_wage"], 10);
+					document.getElementById("Automation").textContent = ROI_breakdown["Automation"];
+					Total_ROI += ROI_breakdown["Automation"];
+
+					ROI_breakdown["Online_Sales"] = online_sales(labeled_input["TPY"], labeled_input["TPrice"], .80);
+					document.getElementById("Online_Sales").textContent = ROI_breakdown["Online_Sales"];
+					Total_ROI += ROI_breakdown["Online_Sales"];
+
+					ROI_breakdown["Email_marketing"] = marketing(labeled_input["Email_marketing"], labeled_input["TPY"]);
+					document.getElementById("Email_marketing").textContent = ROI_breakdown["Email_marketing"];
+					Total_ROI += ROI_breakdown["Email_marketing"];
+
+					ROI_breakdown["Total"] = Total_ROI;
+					document.getElementById("Total_ROI").textContent = Total_ROI;
+				}
+
+				function Festival(labeled_input) {
+
+					ROI_breakdown = new Array();
+					var Total_ROI = 0;
+
+					ROI_breakdown["Ticket_Sales"] = ticket_sales(labeled_input["orig_ROI"]);
+					document.getElementById("Ticket_Sales").textContent = ROI_breakdown["Ticket_Sales"];
+					Total_ROI += ROI_breakdown["Ticket_Sales"];
+
+					ROI_breakdown["Social_Media"] = social_media(labeled_input["TPY"], labeled_input["Type"]);
+					document.getElementById("Social_Media").textContent = ROI_breakdown["Social_Media"];
+					Total_ROI += ROI_breakdown["Social_Media"];
+
+					ROI_breakdown["Online_Sales"] = online_sales(labeled_input["TPY"], labeled_input["TPrice"], .80);
+					document.getElementById("Online_Sales").textContent = ROI_breakdown["Online_Sales"];
+					Total_ROI += ROI_breakdown["Online_Sales"];
+
+					ROI_breakdown["Email_marketing"] = marketing(labeled_input["Email_marketing"], labeled_input["TPY"]);
+					document.getElementById("Email_marketing").textContent = ROI_breakdown["Email_marketing"];
+					Total_ROI += ROI_breakdown["Email_marketing"];
+
+					ROI_breakdown["Total"] = Total_ROI;
+					document.getElementById("Total_ROI").textContent = Total_ROI;
+				}
+
+				function Live_Music(labeled_input) {
+
+					ROI_breakdown = new Array();
+					var Total_ROI = 0;
+
+					ROI_breakdown["Ticket_Sales"] = ticket_sales(labeled_input["orig_ROI"]);
+					document.getElementById("Ticket_Sales").textContent = ROI_breakdown["Ticket_Sales"];
+					Total_ROI += ROI_breakdown["Ticket_Sales"];
+
+					ROI_breakdown["Automation"] = automation(labeled_input["Employee_wage"], 10);
+					document.getElementById("Automation").textContent = ROI_breakdown["Automation"];
+					Total_ROI += ROI_breakdown["Automation"];
+
+					ROI_breakdown["Social_Media"] = social_media(labeled_input["TPY"], labeled_input["Type"]);
+					document.getElementById("Social_Media").textContent = ROI_breakdown["Social_Media"];
+					Total_ROI += ROI_breakdown["Social_Media"];
+
+					ROI_breakdown["Email_marketing"] = marketing(labeled_input["Email_marketing"], labeled_input["TPY"]);
+					document.getElementById("Email_marketing").textContent = ROI_breakdown["Email_marketing"];
+					Total_ROI += ROI_breakdown["Email_marketing"];
+
+					ROI_breakdown["Total"] = Total_ROI;
+					document.getElementById("Total_ROI").textContent = Total_ROI;
+				}
+
 			
 		}
