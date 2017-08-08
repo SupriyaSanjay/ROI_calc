c3d20104931b63f40252c1dfd4d066256c722635
Supriya Sanjay <supriya.sanjay97@gmail.com>
2017-08-08 15:37:49 -0700

    finished, commented

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 #ROI_calc
-#ROI_calc
+Inputs are read into the labeled_inputs array, where indices are:
+["Type", "orig_ROI", "TPY", "TPrice", "Employee_wage", "Email_marketing"] by 
+set_data() and the final array is returned.
+
+The types are then parsed and categorized into 5 larger categories. The
+mapping is as such: 
+-----General-----				
+Arena, Conference Center/Expo
+Casino
+Fair, Attraction, Rodeo
+K-12
+Comedy, Mixed Use Venue
+Museum, Tours
+Other
+Professional Sports
+
+----Live Music---
+Live Music
+Night Club, Promoter
+
+---Festival---
+Festival
+
+---University--
+University 
+
+According to the type, the calculate breakdown function is called
+and the corresponding display function. 
+
+The different display functions are used for different types of
+venues. Since varying venues are concerned with different factors that
+could contribute to ROI, such as donations or social media. Thus, the 
+different functions specify which factors should be printed and which 
+should not. 
+
+The helper functions take certain parameters such as TPY, ticket price, 
+or rates specific to member statistics. The functions then do specific 
+calculations and return the amount of money saved or increase in ROI 
+that the specific factor would have. 
+
+Sources of data used in specific functions:
+Ticket Sales: 16% increase in revenue seen over 4 years in Thalian Hall
+
+Automation: Hours saved (parameter) is standardized to 10, Thalian Hall
+saw box office hours cut by more than 10 hours/weekly 
+
+Social Media: Monetary values of shares and likes on Facebook and Twitter
+for Music/Concerts and Performing Arts Venues. Data from Hubspot. Music/Concert
+applies to General, Live Music, and Festival. Performing Arts applies to only
+Performing Arts venues. 
+
+Marketing: Email Marketing numbers taken from Mailchimp and Constant Contact websites.
+TPY used to roughly determine # of records --> price of email marketing solution.
+
+Donations: Donations numbers taken from Bloomerang's website. TPY used to determine
+# of records ---> price of solution. 
--- ROI_calc.js
+++ ROI_calc.js
@@ -1,9 +1,9 @@
 function Calculate() {
 
-				/************Main*************/
 				var labeled_input = new Array();
 				labeled_input = set_data(); // Set data
 
+				/***Parse venue types, call corresponding calculate and display functions***/
 				// Live Music
 				if ((labeled_input["Type"] == "Night Club, Promoter") || (labeled_input["Type"] == "Live Music")) {
 					labeled_input["Type"] = 2;
@@ -35,9 +35,63 @@ function Calculate() {
 					calculate_breakdown(labeled_input);
 					display_all(labeled_input);
 				}
+				/***************************End of parsing functions*****************/
+
+				// Get data inputted and put in a labeled input array.
+				function set_data() {
+					// Array of fields that will be used as labels
+					var num_fields = 6;
+					var fields = ["Type", "orig_ROI", "TPY", "TPrice", "Employee_wage", "Email_marketing"];
+					var input = new Array()
+					// Loop through document, get inputs and put in labeled array 
+					for (i = 0; i < num_fields; i++) {
+						var field_input = document.getElementById("Revenue")
+						if (i > 0 && i < 4) {
+							input[fields[i]] = accounting.unformat(field_input.elements[i].value);
+						}
+						else {
+							input[fields[i]] = field_input.elements[i].value;	
+						}				
+					}
+					return input;
+				}
+
+				// Calculate_breakdown: Fill in ROI_breakdown array with values, call helpers.
+				function calculate_breakdown(labeled_input) {
+
+					ROI_breakdown = new Array(); // Holds separate aspects of ROI breakdown 
+					var Total_ROI = 0.0;
+
+					ROI_breakdown["Ticket_Sales"] = ticket_sales(labeled_input["orig_ROI"]);
+					document.getElementById("Ticket_Sales").textContent = accounting.formatMoney(ROI_breakdown["Ticket_Sales"]);
+					Total_ROI += ROI_breakdown["Ticket_Sales"];
+
+					ROI_breakdown["Automation"] = automation(labeled_input["Employee_wage"], 10);
+					document.getElementById("Automation").textContent = accounting.formatMoney(ROI_breakdown["Automation"]);
+					Total_ROI += ROI_breakdown["Automation"];
+
+
+					ROI_breakdown["Social_Media"] = social_media(labeled_input["TPY"], labeled_input["Type"]);
+					document.getElementById("Social_Media").textContent = accounting.formatMoney(ROI_breakdown["Social_Media"]);
+					Total_ROI += ROI_breakdown["Social_Media"];
+
+					ROI_breakdown["Online_Sales"] = online_sales(labeled_input["TPY"], labeled_input["TPrice"], .80);
+					document.getElementById("Online_Sales").textContent = accounting.formatMoney(ROI_breakdown["Online_Sales"]);
+					Total_ROI += ROI_breakdown["Online_Sales"];
+
+					ROI_breakdown["Email_marketing"] = marketing(labeled_input["Email_marketing"], labeled_input["TPY"]);
+					document.getElementById("Email_marketing").textContent = accounting.formatMoney(ROI_breakdown["Email_marketing"]);
+					Total_ROI += ROI_breakdown["Email_marketing"];
+
+					ROI_breakdown["Donations"] = donations(labeled_input["TPY"]);
+					document.getElementById("Donations").textContent = accounting.formatMoney(ROI_breakdown["Donations"]);
+					Total_ROI += ROI_breakdown["Donations"];
+
+					ROI_breakdown["Total"] = accounting.formatMoney(Total_ROI);
+					document.getElementById("Total_ROI").textContent = ROI_breakdown["Total"];
+				}
 
 				/***********Display Functions for All Venue Types ************/
-
 				// Display_all: For General and PA venues, displays all fields. 
 				function display_all(labeled_input) {
 					// Holds all possible fields 
@@ -110,72 +164,18 @@ function Calculate() {
 						}
 					}
 				}
-				// Get data inputted and put in a labeled input array.
-				function set_data() {
-					// Array of fields that will be used as labels
-					var num_fields = 6;
-					var fields = ["Type", "orig_ROI", "TPY", "TPrice", "Employee_wage", "Email_marketing"];
-					var input = new Array()
-					// Loop through document, get inputs and put in labeled array 
-					for (i = 0; i < num_fields; i++) {
-						var field_input = document.getElementById("Revenue")
-						if (i > 0 && i < 4) {
-							input[fields[i]] = accounting.unformat(field_input.elements[i].value);
-						}
-						else {
-							input[fields[i]] = field_input.elements[i].value;	
-						}				
-					}
-					return input;
-				}
-				/*******************************************************/
-				
-				// Calculate_breakdown: Fill in ROI_breakdown array with values, call helpers.
-				function calculate_breakdown(labeled_input) {
-
-					ROI_breakdown = new Array(); // Holds separate aspects of ROI breakdown 
-					var Total_ROI = 0.0;
-
-					ROI_breakdown["Ticket_Sales"] = ticket_sales(labeled_input["orig_ROI"]);
-					document.getElementById("Ticket_Sales").textContent = accounting.formatMoney(ROI_breakdown["Ticket_Sales"]);
-					Total_ROI += ROI_breakdown["Ticket_Sales"];
-
-					ROI_breakdown["Automation"] = automation(labeled_input["Employee_wage"], 10);
-					document.getElementById("Automation").textContent = accounting.formatMoney(ROI_breakdown["Automation"]);
-					Total_ROI += ROI_breakdown["Automation"];
-
-
-					ROI_breakdown["Social_Media"] = social_media(labeled_input["TPY"], labeled_input["Type"]);
-					document.getElementById("Social_Media").textContent = accounting.formatMoney(ROI_breakdown["Social_Media"]);
-					Total_ROI += ROI_breakdown["Social_Media"];
-
-					ROI_breakdown["Online_Sales"] = online_sales(labeled_input["TPY"], labeled_input["TPrice"], .80);
-					document.getElementById("Online_Sales").textContent = accounting.formatMoney(ROI_breakdown["Online_Sales"]);
-					Total_ROI += ROI_breakdown["Online_Sales"];
-
-					ROI_breakdown["Email_marketing"] = marketing(labeled_input["Email_marketing"], labeled_input["TPY"]);
-					document.getElementById("Email_marketing").textContent = accounting.formatMoney(ROI_breakdown["Email_marketing"]);
-					Total_ROI += ROI_breakdown["Email_marketing"];
-
-					ROI_breakdown["Donations"] = donations(labeled_input["TPY"]);
-					document.getElementById("Donations").textContent = accounting.formatMoney(ROI_breakdown["Donations"]);
-					Total_ROI += ROI_breakdown["Donations"];
-
-					ROI_breakdown["Total"] = accounting.formatMoney(Total_ROI);
-					document.getElementById("Total_ROI").textContent = ROI_breakdown["Total"];
-				}
+				/***********************End of display functions ***************/
 
 				/************* Helper functions to calculate ROI ***********/
-
-				// Helper function: Calculates ROI from ticket sales
+				// ticket_sales: Calculates ROI from ticket sales
 				function ticket_sales(orig_ROI) {
 					return .16 * orig_ROI;
 				}
-				// Helper function: Calculates hours saved from automation
+				// automation: Calculates hours saved from automation
 				function automation(wage, hrs_saved) {
 					return hrs_saved * wage * 52; 
 				}
-				// Helper function: Calculates ROI from social media
+				// social_media: Calculates ROI from social media
 				function social_media(TPY, type) {
 					if ((type == 2) || (type == 1) || (type == 4)) {
 						return (TPY * (4.49 + 2.29));
@@ -184,12 +184,12 @@ function Calculate() {
 						return (TPY * (4.01 + 0.72));
 					}
 				}
-				// Helper function: Calculates ROI from online sales, because of fee
+				// online_sales: Calculates ROI from online sales, because of fee
 				function online_sales(TPY, TPrice, rate){
 					//tickets sold online have acceptable fee of $6.03
 					return (rate * TPY) * (TPrice + 6.03);
 				}
-				// Helper function: Calculates money saved from email marketing manager
+				// marketing: Calculates money saved from email marketing manager
 				function marketing(email_marketing, TPY){
 					// 1 = Mailchimp 2 = Constant contact 3 = None """
 					if (email_marketing == "Mailchimp") {
@@ -212,7 +212,7 @@ function Calculate() {
 						return 0;
 					}
 				}
-				// Helper functions: Calculates money saved from donations manager 
+				// donations: Calculates money saved from donations manager 
 				function donations(TPY){
 					if (TPY <= 2000) { 
 						return 12 * 99.0;
@@ -227,4 +227,5 @@ function Calculate() {
 						return 12 * 399.0;
 					}
 				}
+				/*****************************End of Helper functions***************/
 		}
